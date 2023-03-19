3c68615ed62b853f4408a08b272aeb694a3c12e3
David Chan <davidchan316@gmail.com>
2023-03-19 19:59:59 -0400

    Added tags into tag table

--- database/food-diary.sql
+++ database/food-diary.sql
@@ -60,7 +60,9 @@ CREATE TABLE entry (
     rating DECIMAL(3,2),
     CONSTRAINT pk_entry PRIMARY KEY(entry_id),
     CONSTRAINT fk_entry_location FOREIGN KEY(location_id) REFERENCES location(location_id),
-    CONSTRAINT fk_entry_restaurant FOREIGN KEY(restaurant_id) REFERENCES restaurant(restaurant_id)
+    CONSTRAINT fk_entry_restaurant FOREIGN KEY(restaurant_id) REFERENCES restaurant(restaurant_id),
+    CONSTRAINT ck_price_gt_0 CHECK (price > 0),
+    CONSTRAINT ck_rating_between_0_and_5 CHECK ((rating >= 0) AND (rating <= 5))
 );
 
 -- diary_entry table
@@ -98,5 +100,95 @@ CREATE TABLE restaurant_tag (
     CONSTRAINT fk_restaurant_tag_restaurant FOREIGN KEY(restaurant_id) REFERENCES restaurant(restaurant_id),
     CONSTRAINT fk_restaurant_tag_tag FOREIGN KEY(tag_id) REFERENCES tag(tag_id)
 );
+
+-- Populate tag table
+-- 1: Price Point
+INSERT INTO tag (tag_type, tag_name) VALUES (1, '$');
+INSERT INTO tag (tag_type, tag_name) VALUES (1, '$$');
+INSERT INTO tag (tag_type, tag_name) VALUES (1, '$$$');
+INSERT INTO tag (tag_type, tag_name) VALUES (1, '$$$$');
+
+-- 2: Category
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'American (New)');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'American (Traditional)');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Asian Fusion');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Bars');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Beer, Wine & Spirits');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Breakfast & Brunch');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Bubble Tea');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Buffets');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Chinese');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Coffee & Tea');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Desserts');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Fast Food');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Food');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Food Delivery Service');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Grocery');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Health Markets');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Italian');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Japanese');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Karaoke');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Korean');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Kosher');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Malaysian');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Nightlife');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Noodles');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Organic Stores');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Pan Asian');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Poke');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Ramen');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Restaurants');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Salad');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Seafood');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Soup');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Specialty Food');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Steakhouses');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Sushi Bars');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Szechuan');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Teppanyaki');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Thai');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Vegan');
+INSERT INTO tag (tag_type, tag_name) VALUES (2, 'Vietnamese');
+
+-- 3: General Features
+INSERT INTO tag (tag_type, tag_name) VALUES (3, 'Accepts Apple Pay');
+INSERT INTO tag (tag_type, tag_name) VALUES (3, 'Accepts Credit Cards');
+INSERT INTO tag (tag_type, tag_name) VALUES (3, 'Cash Only');
+INSERT INTO tag (tag_type, tag_name) VALUES (3, 'Good for Groups');
+INSERT INTO tag (tag_type, tag_name) VALUES (3, 'Good for Kids');
+INSERT INTO tag (tag_type, tag_name) VALUES (3, 'Offers Delivery');
+INSERT INTO tag (tag_type, tag_name) VALUES (3, 'Offers Military Discount');
+INSERT INTO tag (tag_type, tag_name) VALUES (3, 'Offers Takeout');
+INSERT INTO tag (tag_type, tag_name) VALUES (3, 'Open to All');
+INSERT INTO tag (tag_type, tag_name) VALUES (3, 'Outdoor Seating');
+INSERT INTO tag (tag_type, tag_name) VALUES (3, 'Proof of Vaccination Required');
+INSERT INTO tag (tag_type, tag_name) VALUES (3, 'Reservations');
+INSERT INTO tag (tag_type, tag_name) VALUES (3, 'Takes Reservations');
+INSERT INTO tag (tag_type, tag_name) VALUES (3, 'Waiter Service');
+INSERT INTO tag (tag_type, tag_name) VALUES (3, 'Wheelchair Accessible');
+
+-- 4: Alcohol
+INSERT INTO tag (tag_type, tag_name) VALUES (4, 'Beer & Wine Only');
+INSERT INTO tag (tag_type, tag_name) VALUES (4, 'Full Bar');
+INSERT INTO tag (tag_type, tag_name) VALUES (4, 'Good For Happy Hour');
+
+-- 5: Meals Served
+INSERT INTO tag (tag_type, tag_name) VALUES (5, 'Breakfast');
+INSERT INTO tag (tag_type, tag_name) VALUES (5, 'Brunch');
+INSERT INTO tag (tag_type, tag_name) VALUES (5, 'Lunch');
+INSERT INTO tag (tag_type, tag_name) VALUES (5, 'Dinner');
+INSERT INTO tag (tag_type, tag_name) VALUES (5, 'Dessert');
+INSERT INTO tag (tag_type, tag_name) VALUES (5, 'Late Night');
+
+-- 6: Parking
+INSERT INTO tag (tag_type, tag_name) VALUES (6, 'Garage');
+INSERT INTO tag (tag_type, tag_name) VALUES (6, 'Private Lot');
+INSERT INTO tag (tag_type, tag_name) VALUES (6, 'Street');
+INSERT INTO tag (tag_type, tag_name) VALUES (6, 'Valet');
+INSERT INTO tag (tag_type, tag_name) VALUES (6, 'Validated');
+
+-- 7: Wifi
+INSERT INTO tag (tag_type, tag_name) VALUES (7, 'Free');
+INSERT INTO tag (tag_type, tag_name) VALUES (7, 'Paid');
 
 COMMIT;
