7e006aa1c051982f714b8ae4fa38bedc0c4576b5
David Chan <davidchan316@gmail.com>
2023-03-20 01:18:19 -0400

    Added server base code and updated code involving User

---
+++ food-diary-server/src/main/java/com/techelevator/foodiary/dao/UserDao.java
@@ -0,0 +1,18 @@
+package com.techelevator.foodiary.dao;
+
+import com.techelevator.foodiary.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+
+    List<User> findAll();
+
+    User getUserById(int id);
+
+    User findByUsername(String username);
+
+    int findIdByUsername(String username);
+
+    boolean create(String username, String password, String firstName, String lastName);
+}
---
+++ food-diary-server/src/main/java/com/techelevator/foodiary/model/User.java
@@ -0,0 +1,129 @@
+package com.techelevator.foodiary.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class User {
+
+   private int id;
+   private String username;
+   @JsonIgnore // prevent from being sent to client
+   private String password;
+   @JsonProperty("first_name")
+   private String firstName;
+   @JsonProperty("last_name")
+   private String lastName;
+   @JsonIgnore
+   private boolean activated;
+   private Set<Authority> authorities = new HashSet<>();
+
+   public User() { }
+
+   public User(int id, String username, String password, String firstName, String lastName, String authorities) {
+      this.id = id;
+      this.username = username;
+      this.password = password;
+      this.firstName = firstName;
+      this.lastName = lastName;
+      if(authorities != null) this.setAuthorities(authorities);
+      this.activated = true;
+   }
+
+   public int getId() {
+      return id;
+   }
+
+   public void setId(int id) {
+      this.id = id;
+   }
+
+   public String getUsername() {
+      return username;
+   }
+
+   public void setUsername(String username) {
+      this.username = username;
+   }
+
+   public String getPassword() {
+      return password;
+   }
+
+   public void setPassword(String password) {
+      this.password = password;
+   }
+
+   public String getFirstName() {
+      return firstName;
+   }
+
+   public void setFirstName(String firstName) {
+      this.firstName = firstName;
+   }
+
+   public String getLastName() {
+      return lastName;
+   }
+
+   public void setLastName(String lastName) {
+      this.lastName = lastName;
+   }
+
+   public boolean isActivated() {
+      return activated;
+   }
+
+   public void setActivated(boolean activated) {
+      this.activated = activated;
+   }
+
+   public Set<Authority> getAuthorities() {
+      return authorities;
+   }
+
+   public void setAuthorities(Set<Authority> authorities) {
+      this.authorities = authorities;
+   }
+
+   public void setAuthorities(String authorities) {
+      String[] roles = authorities.split(",");
+      for(String role : roles) {
+         this.authorities.add(new Authority("ROLE_" + role));
+      }
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      User user = (User) o;
+      return id == user.id &&
+              activated == user.activated &&
+              Objects.equals(username, user.username) &&
+              Objects.equals(password, user.password) &&
+              Objects.equals(firstName, user.firstName) &&
+              Objects.equals(lastName, user.lastName) &&
+              Objects.equals(authorities, user.authorities);
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(id, username, password, firstName, lastName, activated, authorities);
+   }
+
+   @Override
+   public String toString() {
+      return "User{" +
+              "id=" + id +
+              ", username='" + username + '\'' +
+              ", firstName='" + firstName + '\'' +
+              ", lastName='" + lastName + '\'' +
+              ", activated=" + activated +
+              ", authorities=" + authorities +
+              '}';
+   }
+}
